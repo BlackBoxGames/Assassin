1538ca4c4340af5b45d136cacc9401539c152a5a
dpollan <davidpollan@gmail.com>
2017-04-01 14:22:39 -0500

    Complete User Tests

--- server/dbConfig.js
+++ server/dbConfig.js
@@ -11,7 +11,7 @@ db.connectToDb = () => {
 }
 
 db.disconnectFromDb = () => {
-  mongoose.connection.close();
+  mongoose.disconnect();
 }
 
 
--- server/models/user.js
+++ server/models/user.js
@@ -1,10 +1,68 @@
 const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const Promise = require('bluebird')
+const Schema = mongoose.Schema;
+var db = require('../dbConfig');
+
+
+mongoose.Promise = Promise;
+
  
 const UserSchema = new Schema({
   username: String,
 })
 
-const User = mongoose.model('user', UserSchema);
+var User = mongoose.model('user', UserSchema);
+
+User.findOneUser = function(username) {
+
+  return User.findOne({'username': username})
+  .then((response) => {
+    return response;
+  })
+  .catch(err => {
+    console.error(err);
+  });
+};
+
+User.findAllUsers = function() {
+  return User.find({})
+  .then((response) => {
+    return response;
+  })
+  .catch(err => {
+    console.error(err);
+  });
+};
+
+User.insertUser = function(username) {
+  return User.findOne({username: username})
+  .then((response) => {
+    if (response){
+      //TODO: Add ability to change username 
+    } else {
+      return User.create({username: username})
+      .then(result => {
+      });
+    }
+  })
+  .catch(err => {
+    console.error(err);
+  });
+}
+
+User.deleteUser = function(username) {
+  return User.findOne({username: username})
+  .then((response) => {
+    if (response.username) {
+      return User.deleteOne({username: username})
+    }
+    return response;
+  })
+  .catch(err => {
+    console.error(err);
+  });
+}
+
+
 
 module.exports = User
