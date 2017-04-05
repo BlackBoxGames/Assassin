4a7527c35cb18cb9af52b9e6108bdaffe4269803
Nathan <nathan.turinski@gmail.com>
2017-04-05 12:16:05 -0500

    Rebuilds client folder

--- client/platforms/ios/www/cordova_plugins.js
+++ client/platforms/ios/www/cordova_plugins.js
@@ -9,6 +9,23 @@ module.exports = [
         ]
     },
     {
+        "id": "cordova-plugin-dialogs.notification",
+        "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+        "pluginId": "cordova-plugin-dialogs",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "pluginId": "cordova-plugin-inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
+    },
+    {
         "id": "ionic-plugin-keyboard.keyboard",
         "file": "plugins/ionic-plugin-keyboard/www/ios/keyboard.js",
         "pluginId": "ionic-plugin-keyboard",
@@ -56,6 +73,8 @@ module.exports.metadata =
 {
     "cordova-plugin-whitelist": "1.3.2",
     "cordova-plugin-device": "1.1.5",
+    "cordova-plugin-dialogs": "1.3.2",
+    "cordova-plugin-inappbrowser": "1.7.0",
     "ionic-plugin-keyboard": "2.2.1",
     "cordova-plugin-network-information": "1.3.2",
     "cordova-plugin-splashscreen": "4.0.2",
