b8a7925312b247299182e75652d687aa88fa9747
davos <davidpollan@gmail.com>
2017-04-05 16:51:02 -0500

    Begin implementation of location controller

--- client/app/assassin/controllers/location.js
+++
@@ -1,7 +0,0 @@
-angular.module('assassin.location', [])
-
-// Set up controller for location.
-.controller('LocationCtrl', function($scope, $http, $cordovaGeolocation) {
-  $scope.currentLocation = $cordovaGeolocation.getCurrentPosition()//get coordinates and id from local users phone
-
-}
