6dd7f1afe2b79f4127db78541f17774bbf1aa5e3
davos <davidpollan@gmail.com>
2017-04-10 15:05:38 -0500

    Refactor user-crtl and user template, backed up ionic version

--- client/app/main/controllers/user-ctrl.js
+++ client/app/main/controllers/user-ctrl.js
@@ -1,10 +1,9 @@
 'use strict';
 angular.module('main')
-.controller('UserCtrl', function ($log, $ionicAuth) {
+.controller('UserCtrl', function ($log, $http) {
 
   this.user = {
-    email: '',
-    password: ''
+    username: ''
   };
   this.updateResult = function (type, result) {
     $log.log(type, result);
@@ -12,24 +11,31 @@ angular.module('main')
     this.user.result = result;
   };
 
-  var responseCB = function (response) {
-    this.updateResult('Response', response);
-  }.bind(this);
-
-  var rejectionCB = function (rejection) {
-    this.updateResult('Rejection', rejection);
-  }.bind(this);
-
   // tries to sign the user up and displays the result in the UI
   this.signup = function () {
-    $ionicAuth.signup(this.user)
-    .then(responseCB)
-    .catch(rejectionCB);
+    $http({
+      method: 'PUT',
+      url: 'http://localhost:4000/users',
+      data: this.user
+    }).then(function (response) {
+      console.log(response);
+    }, function (err) {
+      console.error(err);
+    });
   };
+  // very basic MVP, if user exists it just overwrites, TODO: check first and redirect to signin if user already exists.
   // tries to sign in the user and displays the result in the UI
-  this.signin = function () {
-    $ionicAuth.login('basic', this.user)
-    .then(responseCB)
-    .catch(rejectionCB);
+
+  this.signin = function() {
+    console.log(this.user.username);
+    console.log('http://localhost:4000/users?username=' + this.user.username);
+    $http({
+      method: 'GET',
+      url: 'http://localhost:4000/users?username=' + this.user.username
+    }).then(function (response) {
+      console.log(response);
+    }, function (err) {
+      console.error(err);
+    });
   };
 });
