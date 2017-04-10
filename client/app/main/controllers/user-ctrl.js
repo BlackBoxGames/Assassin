bd9b482e8509c14ba93433855240b08ac19fd79c
davos <davidpollan@gmail.com>
2017-04-10 17:28:03 -0500

    Implement user login logic TODO: add and implement Google Oauth for authentication

--- client/app/main/controllers/user-ctrl.js
+++ client/app/main/controllers/user-ctrl.js
@@ -1,39 +1,47 @@
 'use strict';
 angular.module('main')
-.controller('UserCtrl', function ($log, $http) {
+.controller('UserCtrl', function ($log, $http, $scope) {
+
+  $scope.announcer = '';
 
   this.user = {
     username: ''
   };
-  this.updateResult = function (type, result) {
-    $log.log(type, result);
-    this.user.resultType = type;
-    this.user.result = result;
-  };
-
   // tries to sign the user up and displays the result in the UI
   this.signup = function () {
-    $http({
-      method: 'PUT',
-      url: 'http://localhost:4000/users',
-      data: this.user
-    }).then(function (response) {
-      console.log(response);
-    }, function (err) {
-      console.error(err);
-    });
-  };
-  // very basic MVP, if user exists it just overwrites, TODO: check first and redirect to signin if user already exists.
-  // tries to sign in the user and displays the result in the UI
-
-  this.signin = function() {
-    console.log(this.user.username);
-    console.log('http://localhost:4000/users?username=' + this.user.username);
     $http({
       method: 'GET',
       url: 'http://localhost:4000/users?username=' + this.user.username
     }).then(function (response) {
-      console.log(response);
+      if (response.status === 404) {
+        $http({
+          method: 'PUT',
+          url: 'http://localhost:4000/users',
+          data: this.user
+        }).then(function (response) {
+          console.log(response); //for linter
+        }, function (err) {
+          console.error(err);
+        });
+      } else {
+        $scope.announcer = 'That username is already taken.';
+      }
+    });
+  };
+
+  this.signin = function() {
+    $http({
+      method: 'GET',
+      url: 'http://localhost:4000/users?username=' + this.user.username
+    }).then(function (response) {
+      if (response.status === 200) {
+        //authenticate the user TODO: add google Auth stuff
+        $scope.announcer = 'Logged in as ' + this.user.username;
+      } else {
+        //Prompt user to create a new account
+        $scope.announcer = 'User  does not exist, click the sign up button to create a new account';
+        console.log($scope.announcer);
+      }
     }, function (err) {
       console.error(err);
     });
