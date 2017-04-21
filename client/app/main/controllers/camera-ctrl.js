8859741eacf024854c48fe62275b81e3e40a0f34
dpollan <davidpollan@gmail.com>
2017-04-21 14:22:04 -0500

    Prevent user from accessing camera before having target

--- client/app/main/controllers/camera-ctrl.js
+++ client/app/main/controllers/camera-ctrl.js
@@ -3,11 +3,19 @@ angular.module('main')
 .controller('CameraCtrl', function ($scope, $rootScope, $http, $cordovaDevice, $cordovaCamera) {
   $scope.takePhoto = function () {
     if (!$rootScope.loggedIn) {
+<<<<<<< f768a859d07b51c36717cd7683d38db229ad6ab2
 <<<<<<< 6df9574d0d99f0f8a0cf01cfa39a375704cfd878
       $rootScope.$emit('rootScope: toggleFail');
 =======
       $rootScope.$emit('rootScope: cameraFail');
 >>>>>>> Add rootscope listener and alert for cameraFail if not logged in
+=======
+      $rootScope.$emit('rootScope: toggleFail');
+      return;
+    }
+    if (!$rootScope.hasTarget) {
+      $rootScope.$emit('rootScope: cameraNoTarget');
+>>>>>>> Prevent user from accessing camera before having target
       return;
     }
     var options = {
